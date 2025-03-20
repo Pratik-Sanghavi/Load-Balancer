6ee1e52c67c7a96b2cf8a27fa507b676d43255e6
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2025-03-20 12:21:02 -0500

    Make directory before mounting

--- profiles/profile.py
+++ profiles/profile.py
@@ -44,10 +44,14 @@ load_balancer.disk_image = params.osImage
 load_balancer.addService(rspec.Execute(shell="sh", command="sudo apt-get update && sudo apt-get install -y docker.io"))
 
 # Nginx Load Balancer
+load_balancer.addService(rspec.Execute(shell="sh", command="mkdir /users/prsangh/logs/nginx"))
+load_balancer.addService(rspec.Execute(shell="sh", command="mkdir /users/prsangh/nginx_config"))
 load_balancer.addService(rspec.Execute(shell="sh", command="wget -O /users/prsangh/nginx_config/nginx.conf https://raw.githubusercontent.com/Pratik-Sanghavi/Load-Balancer/main/nginx_config/nginx.conf"))
 load_balancer.addService(rspec.Execute(shell="sh", command="sudo docker run -d --name nginx-lb -p 80:80 -v /users/prsangh/logs/nginx:/var/log/nginx -v /users/prsangh/nginx_config/nginx.conf:/etc/nginx/nginx.conf {}".format(params.nginxDocker)))
 
 # HAProxy Load Balancer
+load_balancer.addService(rspec.Execute(shell="sh", command="mkdir /users/prsangh/logs/haproxy"))
+load_balancer.addService(rspec.Execute(shell="sh", command="mkdir /users/prsangh/haproxy_config"))
 load_balancer.addService(rspec.Execute(shell="sh", command="wget -O /users/prsangh/haproxy_config/haproxy.cfg https://raw.githubusercontent.com/Pratik-Sanghavi/Load-Balancer/main/nginx_config/haproxy.cfg"))
 load_balancer.addService(rspec.Execute(shell="sh", command="sudo docker run -d --name haproxy-lb -p 81:80 -p 8404:8404 -v /users/prsangh/logs/haproxy:/usr/local/etc/haproxy -v /users/prsangh/haproxy_config/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro {}".format(params.haproxyDocker)))
 
